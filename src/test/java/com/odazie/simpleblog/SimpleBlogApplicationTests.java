1a8d149f9bbe29e3281851b02084b079f6829bfe
KDN User <the2sang@gmail.com>
2022-05-01 16:22:26 +0900

    configuration

--- src/test/java/com/odazie/simpleblog/SimpleBlogApplicationTests.java
+++ src/test/java/com/odazie/simpleblog/SimpleBlogApplicationTests.java
@@ -3,10 +3,10 @@ package com.odazie.simpleblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SimpleBlogApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
