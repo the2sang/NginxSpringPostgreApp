015501e4df86ae80ba80c650905d346766e8f59d
KHC <the2sang@gmail.com>
2022-05-05 09:46:08 +0900

    add login form and security configuration

--- src/test/java/com/shop/service/MemeberServiceTest.java
+++ src/test/java/com/shop/service/MemeberServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.TestPropertySource;
 import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -44,5 +45,19 @@ public class MemeberServiceTest {
         assertEquals(member.getAddress(), savedMemeber.getAddress());
         assertEquals(member.getPassword(), savedMemeber.getPassword());
         assertEquals(member.getRole(), savedMemeber.getRole());
+    }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicatedMemberTest() {
+        Member member1  = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);
+        });
+
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
     }
 }
