05135a7b674f9370000dbb4175c600f2e942b58e
the2sang <the2sang@gmail.com>
2022-05-05 15:33:36 +0900

    add entity relation and lazy loading

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,39 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
+}
